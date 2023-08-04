fd4852d0dc65b4538a467bed632e46b6436c2152
Vernon Pitney <vernon@pitney.us>
2023-08-03 19:09:59 -0700

    Add project files.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Pong
+{
+    partial class Pong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pong));
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.pongBall = new System.Windows.Forms.PictureBox();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.White;
+            this.player1.Location = new System.Drawing.Point(12, 340);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(20, 120);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            this.player1.Click += new System.EventHandler(this.player1_Click);
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.White;
+            this.player2.Location = new System.Drawing.Point(952, 340);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(20, 120);
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            this.player2.Click += new System.EventHandler(this.player2_Click);
+            // 
+            // pongBall
+            // 
+            this.pongBall.BackColor = System.Drawing.Color.Transparent;
+            this.pongBall.Image = global::Pong.Properties.Resources.pong_ball;
+            this.pongBall.Location = new System.Drawing.Point(480, 380);
+            this.pongBall.Name = "pongBall";
+            this.pongBall.Size = new System.Drawing.Size(40, 40);
+            this.pongBall.TabIndex = 2;
+            this.pongBall.TabStop = false;
+            this.pongBall.Click += new System.EventHandler(this.pongBall_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("OCR A Extended", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(408, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(188, 66);
+            this.title.TabIndex = 3;
+            this.title.Text = "Pong";
+            this.title.Click += new System.EventHandler(this.title_Click);
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.pongBall);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.title);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pong";
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Pong_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.PictureBox pongBall;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Pong : Form
+    {
+        byte playerOneScore = 0;
+        byte playerTwoScore = 0;
+        short paddlePositionY1 = 340;
+        short paddlePositionY2 = 340;
+        short paddlePositionX1 = 12;
+        short paddlePositionX2 = 952;
+        byte paddleSpeed = 10;
+        Thread gameThread;
+
+        bool isPlayer1MovingUp = false;
+        bool isPlayer1MovingDown = false;
+        bool isPlayer2MovingUp = false;
+        bool isPlayer2MovingDown = false;
+
+        public Pong()
+        {
+            InitializeComponent();
+            StartGameThread();
+        }
+
+        void GameLoop()
+        {
+            while (gameThread != null)
+            {
+                Update();
+                try
+                {
+                    Thread.Sleep(16);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    Console.WriteLine("Error: Game loop has stopped working");
+                }
+            }
+        }
+
+        new void Update()
+        {
+            if (isPlayer1MovingUp)
+            {
+                paddlePositionY1 -= paddleSpeed;
+            }
+            if (isPlayer1MovingDown)
+            {
+                paddlePositionY1 += paddleSpeed;
+            }
+            if (isPlayer2MovingUp)
+            {
+                paddlePositionY2 -= paddleSpeed;
+            }
+            if (isPlayer2MovingDown)
+            {
+                paddlePositionY2 += paddleSpeed;
+            }
+
+            if (paddlePositionY1 < 0)
+            {
+                paddlePositionY1 = 0;
+            }
+            if (paddlePositionY1 + 160 > 800)
+            {
+                paddlePositionY1 = 800 - 160;
+            }
+
+            if (paddlePositionY2 < 0)
+            {
+                paddlePositionY2 = 0;
+            }
+            if (paddlePositionY2 + 160 > 800)
+            {
+                paddlePositionY2 = 800 - 160;
+            }
+
+
+            // Update the paddle positions in the UI on the main UI thread
+            UpdatePaddlePositions();
+        }
+
+        void UpdatePaddlePositions()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)UpdatePaddlePositions);
+            }
+            else
+            {
+                // Update the paddle positions in the UI on the main UI thread
+                player1.Location = new Point(paddlePositionX1, paddlePositionY1);
+                player2.Location = new Point(paddlePositionX2, paddlePositionY2);
+            }
+        }
+
+        void StartGameThread()
+        {
+            gameThread = new Thread(GameLoop);
+            gameThread.Start();
+        }
+
+        private void Pong_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void player1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pongBall_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void player2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.W)
+            {
+                isPlayer1MovingUp = true;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                isPlayer1MovingDown = true;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                isPlayer2MovingUp = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                isPlayer2MovingDown = true;
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            if (e.KeyCode == Keys.W)
+            {
+                isPlayer1MovingUp = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                isPlayer1MovingDown = false;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                isPlayer2MovingUp = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                isPlayer2MovingDown = false;
+            }
+        }
+    }
+}
